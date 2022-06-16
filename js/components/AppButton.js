d4486f2f1a09f1ee08ddea8a88f6b7c6885962f9
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-06-16 18:31:48 -0400

    e6 - Component Props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,11 +1,26 @@
 export default {
   template: `
-            <button class="bg-slate-500 hover:bg-slate-800 border rounded px-5 py-2 text-white disabled:cursor-not-allowed" :disabled="processing"> 
+            <button 
+            :class="{ 'border rounded px-5 py-2  disabled:cursor-not-allowed' : true,
+            'bg-cyan-500 hover:bg-cyan-800': type === 'primary', 
+            'bg-amber-500 hover:bg-amber-800': type === 'secondary', 
+            'bg-gray-500 hover:bg-slate-800': type === 'muted', 
+            'is-loading' : processing
+            }"
+            :disabled="processing"
+            > 
               <slot /> 
-            </button>`,
-  data() {
-    return {
-      processing: true,
-    };
+            </button>
+            `,
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+    },
+
+    processing: {
+      type: Boolean,
+      default: false,
+    },
   },
 };
